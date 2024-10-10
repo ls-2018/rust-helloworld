5e1196d245ff6c1bb45edc2a0a5ad1a5aeca3fd5
刘硕 <liushuo@zetyun.com>
2024-10-10 13:49:47 +0800

    Thu Oct 10 13:49:47 CST 2024

--- garden/src/lib.rs
+++ garden/src/lib.rs
@@ -1,13 +1,47 @@
-#[allow(unused_imports)]
-#[allow(unused_variables)]
-#[allow(unreachable_patterns)]
-#[allow(unused_mut)]
-#[allow(non_snake_case)]
-#[allow(unused_must_use)]
-#[allow(dead_code)]
-#[allow(unused_assignments)]
-pub mod garden;
-pub use garden::*;
+mod add;
+mod base;
+mod class;
+mod debug;
+mod display;
+mod env;
+#[macro_use]
+mod file;
+mod err;
+mod r#for;
+mod from;
+mod r#if;
+mod io;
+mod iter;
+mod r#match;
+mod module;
+mod panic;
+mod parse;
+mod pbj;
+mod random;
+mod rc;
+mod r#ref;
+mod reg;
+mod safe;
+mod str;
+mod r#trait;
+mod trait_asref;
+mod trait_bibao;
+mod trait_copy;
+mod trait_default;
+mod trait_deref;
+mod trait_drop;
+mod r#type;
+mod vec;
+mod vegetables;
+mod web;
+mod width_ref;
 
-// ! wasd asd
-fn main() {}
+// #[allow(unused_imports)]
+// #[allow(unused_variables)]
+// #[allow(unreachable_patterns)]
+// #[allow(unused_mut)]
+// #[allow(non_snake_case)]
+// #[allow(unused_must_use)]
+// #[allow(dead_code)]
+// #[allow(unused_assignments)]
+// pub use file::write_log_entry;
--- rust-blog/assets/main.rs
+++
@@ -1,3 +0,0 @@
-fn main() {
-    println!("hopefully github detects this file and categorizes my repo as a Rust repo");
-}
